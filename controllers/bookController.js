cd72b40032d640d52348f2426420253a5bd2e814
sakib404 <62664582+sakib404@users.noreply.github.com>
2022-10-24 09:33:49 +0600

    Add files via upload

---
+++ controllers/bookController.js
@@ -0,0 +1,75 @@
+const bookModel = require("../models/books")
+
+
+const getBookList = async (req,res) =>{
+    let data= []
+    let books = []
+    try{
+        data = await bookModel.find()
+        
+        data.forEach((bookSchema)=>{
+            books.push({id: bookSchema.id, name: bookSchema.name, author: bookSchema.author, genre: bookSchema.genre})
+        })
+    }catch(err){
+        console.log(err)
+    }finally{
+        res.render("bookList",{ books: books, txt: "Hello!"})
+    }
+}
+
+const addBook = (req, res) =>{
+    res.render("addBooks")
+}
+
+const postBook = (req, res) =>{
+    const data = new bookModel({
+        name: req.body.name,
+        author: req.body.author,
+        genre: req.body.genre
+    })
+    data.save().then(()=>{
+        console.log("Book Added!")
+    }).catch((error)=>{console.log(error)}).finally(()=>{
+        res.redirect("/book-list")
+    })
+    
+}
+
+const deleteBook = (req, res) =>{
+    const deleteId = req.params.id
+    bookModel.findOneAndDelete({_id : deleteId}).then(()=>{
+        console.log("Book Deleted! id:" + deleteId)
+    }).catch((error)=>{console.log(error)}).finally(()=>{
+        res.redirect("/book-list")
+    })
+}
+
+const showEditBook = (req,res) => {
+    const editId = req.params.id
+    bookModel.findById(editId, function (err, book) {
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.render("editBook", {book: book})
+        }
+    })
+}
+
+const editBook = (req,res) => {
+    editId = req.params.id
+    bookModel.findByIdAndUpdate(editId,{
+        name: req.body.name,
+        author: req.body.author,
+        genre: req.body.genre
+    },(err)=>{
+        if (err){
+            console.log(err)
+        }
+        else{
+            res.redirect("/book-list")
+        }
+    })
+}
+
+module.exports= {getBookList, addBook, postBook, deleteBook, showEditBook, editBook}
